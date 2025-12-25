1df697a764b6740de62e4b55464bae567391ea31
MalikUmais <155088514+MalikUmais@users.noreply.github.com>
2025-12-25 20:24:15 +0500

    api url fix

--- js/api/productApi.js
+++ js/api/productApi.js
@@ -1,4 +1,4 @@
-const API_URL="https://wad-project-backend-n3tlu3f9e-malikumais-projects.vercel.app/";
+const API_URL="https://wad-project-backend-n3tlu3f9e-malikumais-projects.vercel.app";
 export async function getProducts() {
     try {
         const response = await fetch(`${API_URL}/products`);
--- js/api/reviewApi.js
+++ js/api/reviewApi.js
@@ -1,4 +1,4 @@
-const API_URL="https://wad-project-backend-n3tlu3f9e-malikumais-projects.vercel.app/";
+const API_URL="https://wad-project-backend-n3tlu3f9e-malikumais-projects.vercel.app";
 export async function getReviews() {
     try {
         const response = await fetch(`${API_URL}/reviews`);
